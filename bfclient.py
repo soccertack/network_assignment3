270cc881ce56bd3fd1b2cf5c45d6825e2fa644cf
Jintack <jintack@cs.columbia.edu>
2015-11-29 18:43:52 -0500

    Cleanup. Use public IP.

--- bfclient.py
+++ bfclient.py
@@ -6,21 +6,34 @@ import struct
 from collections import defaultdict
 
 class node:
-	def __init__(self, IP, port, dist):
+	def __init__(self, IP, port):
 		self.IP = IP
 		self.port = port
-		self.dist = dist
 
-HOST = 'localhost'
+HOST = ''
 neighbors = []
+
+header_struct = struct.Struct('H')
 # recv_port_number(H, 2B). command(s). IP(s). Port(H). dist(f)
 update_struct = struct.Struct('H 10s 15s H f')
 dv = defaultdict(dict)
 
+#http://stackoverflow.com/questions/24196932/how-can-i-get-the-ip-address-of-eth0-in-python
+
+def get_ip_address():
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	s.connect(("8.8.8.8", 80))
+	return s.getsockname()[0]  
+
 def make_update_pkt(recv_port, cmd, IP, remote_port, dist):
 	values = (recv_port, cmd, IP, remote_port, dist)
-	print "make_update_pkt ", IP
 	return update_struct.pack(*values)
+
+def add_neighbor(IP, port):
+	x = node(IP, port)
+	if (IP, port) not in neighbors:
+		print 'add ', IP, port
+		neighbors.append((IP, port))	
 
 def handle_input(argv):
 	argc = len(argv)
@@ -34,8 +47,7 @@ def handle_input(argv):
 		port = int(argv[arg_idx+1])
 		dist = float(argv[arg_idx+2])
 
-		x = node(argv[arg_idx], int(argv[arg_idx+1]), float(argv[arg_idx+2]))
-		neighbors.append(x)	
+		add_neighbor(IP, port)
 
 		dv[('localhost', myport)][(IP, port)] = dist
 		arg_idx += 3
@@ -58,6 +70,9 @@ def init_socket(myport):
 	return s
 
 def make_mydv(myport):
+#	values = (myport)
+#	header = header_struct.pack(*values)
+	header = ''
 	pkt =''
 	for node in dv[('localhost', myport)]:
 		IP = node[0]
@@ -65,17 +80,33 @@ def make_mydv(myport):
 		dist = dv[('localhost', myport)][node]
 		tmp_pkt = make_update_pkt(myport, "UPDATE", IP, port, dist)
 		pkt += tmp_pkt
-	return pkt
+	return header+pkt
+
+# Send my information to other nodes
+def route_update(myport, send_socket):
+	pkt = make_mydv(myport)
+	for IP, port in neighbors:
+		send_socket.sendto(pkt, (IP, port));
+
+def handle_pkt(myport, d, sender_IP):
+	idx = 0
+	data_len = len(d)
+
+	while idx < data_len:
+		data = d[idx:idx+36]
+		idx += 36
+		(recv_port, cmd, IP, remote_port, dist) = update_struct.unpack(data)
+		print recv_port ,cmd, IP, remote_port, dist
+		IP = IP.rstrip('\0')
+		dv[('localhost',myport)][(IP, recv_port)] = dist
+
+	add_neighbor(sender_IP, recv_port)
 
 def main():
 	myport = handle_input(sys.argv)
 	recv_socket = init_socket(myport)
 	send_socket = init_socket(0)
-
-	pkt = make_mydv(myport)
-	# Send my information to other nodes
-	for neighbor in neighbors:
-		send_socket.sendto(pkt, (neighbor.IP, neighbor.port));
+	route_update(myport, send_socket)
 
 	socket_list = [sys.stdin, recv_socket]
 	while 1:
@@ -84,19 +115,8 @@ def main():
 			#incoming message from remote server
 			if sock == recv_socket:
 				d = recv_socket.recvfrom(1024)
-				print "len is ", len(d[0])
-				idx = 0
-
-				while idx < len(d[0]):
-					data = d[0][idx:idx+36]
-					idx += 36
-					(recv_port, cmd, IP, remote_port, dist) = update_struct.unpack(data)
-					print recv_port ,cmd, IP, remote_port, dist
-					IP = IP.rstrip('\0')
-					x = node(IP, recv_port, dist)
-					neighbors.append(x)	
-					dv[('localhost',myport)][(IP, recv_port)] = dist
-
+				sender_IP = d[1][0]
+				handle_pkt(myport, d[0], sender_IP)
 				'''
 				update_dv()	
 				need_notify = calc_dv()
@@ -105,11 +125,8 @@ def main():
 				'''
 			#user entered a message
 			else :
-				pkt = make_mydv(myport)
 				msg = raw_input()
-				for neighbor in neighbors:
-					send_socket.sendto(pkt, (neighbor.IP, neighbor.port));
-					print 'send to ', neighbor.IP, neighbor.port, neighbor.dist
+				route_update(myport, send_socket)
 
 
 if __name__ == '__main__':
